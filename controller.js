c1fc9878afc3b96ccf96d5535b86ca5468c2309b
Jonathan Stewmon <jstewmon@gmail.com>
2012-01-06 11:40:14 -0600

    added email, templating, and configuration modules

--- controller.js
+++ controller.js
@@ -1,4 +1,28 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
+var nconf = require('nconf');
+var nodemailer = require('nodemailer');
+var handlebars = require('handlebars');
+
+
+function resolve() {
+	var args = arguments;
+	Array.prototype.unshift.call(args, __dirname);
+	return path.resolve.apply(path, args);
+}
+
+function loadTemplateFile(name, cb) {
+	fs.readFile(path.join(resolve('./templates'), name), function (err, data) {
+	  if (err) cb(err);
+		else cb(null, data);;
+	});
+}
+
+// one time action to set up SMTP information
+nodemailer.SMTP = {
+    host: 'localhost'
+};
 
 var app = express.createServer();
 
@@ -9,7 +33,38 @@ app.configure(function() {
 
 app.post('/post-receive', function(req, res) {
 	console.log(req.body);
-	res.send(req.body);
+	var payload = JSON.parse(req.body.payload);
+	res.send('i parsed you');
+	
+	loadTemplateFile('post-receive.txt', function loadedTemplate(err, source) {
+		if(err) {
+			console.error(err);
+		}
+		else {
+			var template = handlebars.compile(source);
+			var output = template(req.body);
+			// send an e-mail
+			nodemailer.send_mail(
+			  // e-mail options
+		    {
+		        sender: 'jstewmon@gmail.com',
+		        to:'jstewmon@glgroup.com',
+		        subject:'post-receive hook called',
+		        //html: '<p><b>Hi,</b> how are you doing?</p>',
+		        body: output
+		    },
+		    // callback function
+		    function(err, success) {
+					if(err) {
+						console.error('Failed to send message...');
+						console.error(err);
+					}
+					else {
+						console.log('Message sent');
+					}
+		    });
+		}
+	});
 });
 
 app.get('*', function(req, res) {
