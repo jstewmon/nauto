9d9eb8605e8ce354dd0e6311570993f25cc31e4e
Jonathan Stewmon <jstewmon@gmail.com>
2012-03-08 13:53:30 -0600

    commented user creation

--- deployment/ubuntu.sh
+++ deployment/ubuntu.sh
@@ -5,9 +5,9 @@
 #----------------------------------------------------------
 # Settings
 #----------------------------------------------------------
-if [ "$NAUTO_USER" = '' ]; then echo "Assigning NAUTO_USER=nauto"; NAUTO_USER=nauto; fi;
-if [ "$NAUTO_GROUP" = '' ]; then echo "Assigning NAUTO_GROUP=nogroup"; NAUTO_GROUP=nogroup; fi;
-if [ "$NAUTO_DIR" = '' ]; then echo "Assigning NAUTO_DIR=/var/nauto"; NAUTO_DIR=/var/$NAUTO_USER; fi;
+# if [ "$NAUTO_USER" = '' ]; then echo "Assigning NAUTO_USER=nauto"; NAUTO_USER=nauto; fi;
+# if [ "$NAUTO_GROUP" = '' ]; then echo "Assigning NAUTO_GROUP=nogroup"; NAUTO_GROUP=nogroup; fi;
+# if [ "$NAUTO_DIR" = '' ]; then echo "Assigning NAUTO_DIR=/var/nauto"; NAUTO_DIR=/var/$NAUTO_USER; fi;
 
     
 #----------------------------------------------------------
@@ -19,16 +19,16 @@ echo ------------------------------------------------------
 apt-get update -y
 apt-get install make libssl-dev build-essential curl git-core -y
 
-# create application account if needed
-echo -e "\nSetting up accounts ...."
-echo ------------------------------------------------------
-echo "adduser --system --home $NAUTO_DIR $NAUTO_USER"
-adduser --system --home $NAUTO_DIR $NAUTO_USER
-chown $NAUTO_USER:$NAUTO_GROUP $NAUTO_DIR
-chmod 777 $NAUTO_DIR
+# # create application account if needed
+# echo -e "\nSetting up accounts ...."
+# echo ------------------------------------------------------
+# echo "adduser --system --home $NAUTO_DIR $NAUTO_USER"
+# adduser --system --home $NAUTO_DIR $NAUTO_USER
+# chown $NAUTO_USER:$NAUTO_GROUP $NAUTO_DIR
+# chmod 777 $NAUTO_DIR
 
-echo ------------------------------------------------------
-echo -e "\nDone!"
+# echo ------------------------------------------------------
+# echo -e "\nDone!"
 
 
 
